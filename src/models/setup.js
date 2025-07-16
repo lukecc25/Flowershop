15b00f75858256207e3ac1cc4ef9333871495049
lukecc25 <cam23019@byui.edu>
2025-07-16 05:09:39 -0600

    fixed inconsistency in the flower data it was called product_id instead of flower_id

--- src/models/setup.js
+++ src/models/setup.js
@@ -94,70 +94,70 @@ const adminUser = {
  */
 const sampleFlowers = [
     {
-        product_id: 1,
+        flower_id: 1,
         name: 'Azalea',
         category: 'Shrubs',
         price: 15.95,
         photo: 'https://cdn.pixabay.com/photo/2018/05/12/19/24/azalea-3394380_1280.jpg'
     },
     {
-        product_id: 2,
+        flower_id: 2,
         name: 'Camellia',
         category: 'Shrubs',
         price: 18.95,
         photo: 'https://cdn.pixabay.com/photo/2022/05/02/21/43/camellia-7170471_1280.jpg'
     },
     {
-        product_id: 3,
+        flower_id: 3,
         name: 'Gardenia',
         category: 'Shrubs',
         price: 19.95,
         photo: 'https://cdn.pixabay.com/photo/2020/04/27/02/41/gardenia-5097886_1280.jpg'
     },
     {
-        product_id: 4,
+        flower_id: 4,
         name: 'Hibiscus',
         category: 'Shrubs',
         price: 25.95,
         photo: 'https://cdn.pixabay.com/photo/2021/11/10/14/25/hibiscus-6784088_1280.jpg'
     },
     {
-        product_id: 5,
+        flower_id: 5,
         name: 'Lantana',
         category: 'Shrubs',
         price: 22.95,
         photo: 'https://cdn.pixabay.com/photo/2023/06/02/23/44/flower-8036709_1280.jpg'
     },
     {
-        product_id: 6,
+        flower_id: 6,
         name: 'Nandina',
         category: 'Shrubs',
         price: 16.95,
         photo: 'https://cdn.pixabay.com/photo/2021/11/09/16/59/nandina-domestica-6781943_1280.jpg'
     },
     {
-        product_id: 7,
+        flower_id: 7,
         name: 'Begonia',
         category: 'Container Plants',
         price: 12.95,
         photo: 'https://cdn.pixabay.com/photo/2017/04/27/04/45/begonia-flower-2264432_1280.jpg'
     },
     {
-        product_id: 8,
+        flower_id: 8,
         name: 'Coleus',
         category: 'Container Plants',
         price: 9.95,
         photo: 'https://cdn.pixabay.com/photo/2023/05/14/23/49/leaves-7993903_1280.jpg'
     },
     {
-        product_id: 9,
+        flower_id: 9,
         name: 'Geranium',
         category: 'Container Plants',
         price: 11.95,
         photo: 'https://cdn.pixabay.com/photo/2016/06/06/12/04/geranium-1439280_1280.jpg'
     },
     {
-        product_id: 10,
+        flower_id: 10,
         name: 'Hydrangea',
         category: 'Herbaceous Perennials',
         price: 8.95,
@@ -187,9 +187,9 @@ const insertAdminUser = `
  * SQL to insert flower records
  */
 const insertSampleFlowers = `
-    INSERT INTO flowers (product_id, name, category, price, photo)
+    INSERT INTO flowers (flower_id, name, category, price, photo)
     VALUES ($1, $2, $3, $4, $5)
-    ON CONFLICT (product_id) DO NOTHING;
+    ON CONFLICT (flower_id) DO NOTHING;
 `;
 
 /**
@@ -223,7 +223,7 @@ const setupDatabase = async () => {
         if (verbose) console.log('Inserting flower data...');
         for (const flower of sampleFlowers) {
             await db.query(insertSampleFlowers, [
-                flower.product_id,
+                flower.flower_id,
                 flower.name,
                 flower.category,
                 flower.price,
